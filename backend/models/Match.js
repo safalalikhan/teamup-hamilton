41f100c30df83c3c9e755dec88245ff78cd04b91
Benit Babu <benitbabu@Benits-MacBook-Pro.local>
2025-07-28 16:27:52 +1200

    initial setup and models

---
+++ backend/models/Match.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const matchSchema = new mongoose.Schema({
+  date: { type: Date, required: true },
+  time: String,
+  turf: { type: mongoose.Schema.Types.ObjectId, ref: 'Turf' },
+  players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  teams: {
+    teamA: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    teamB: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  },
+  status: {
+    type: String,
+    enum: ['Scheduled', 'Completed', 'Cancelled'],
+    default: 'Scheduled'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Match', matchSchema);
